ae47e04302e21fe75665de730482998263cf7e9f
Jose Lombana <jlombana@EN911066.local>
2017-10-06 10:18:03 -0500

    change state to the constructor

--- src/App.js
+++ src/App.js
@@ -6,26 +6,20 @@ import { getAll, search } from './BooksAPI'
 import Category from './components/category';
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    allBooks: {},
-    currentlyReading: [],
-    wantToRead: [],
-    read: [],
-    showSearchPage: false,
-    searchValue: '',
-    searchBooks: [],
-  }
-  
   constructor(props) {
     super(props);
     this.changeCategory = this.changeCategory.bind(this);
     this.onTextCahange = this.onTextCahange.bind(this);
+
+    this.state = {
+      allBooks: {},
+      currentlyReading: [],
+      wantToRead: [],
+      read: [],
+      showSearchPage: false,
+      searchValue: '',
+      searchBooks: [],
+    }
   }
   
   objectBooks(res) {
